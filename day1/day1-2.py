352e65f0da83b4d0b62551802f2b7de83c3a67d5
Claud10R <claudio.ruocco94@gmail.com>
2020-01-17 10:06:41 +0100

    day1-2 pep8 cleanup

--- day1/day1-2.py
+++ day1/day1-2.py
@@ -1,21 +1,25 @@
-import math, os
+import math
+import os
 fileDir = os.path.dirname(os.path.abspath(__file__))
 
-def calcFuel(x):
-	newPeso = math.floor(x/3) - 2
 
-	if newPeso <= 0:
+def calc_fuel(x):
+	new_peso = math.floor(x/3) - 2
+
+	if new_peso <= 0:
 		return 0
 
-	return newPeso + calcFuel(newPeso)
+	return new_peso + calc_fuel(new_peso)
+
 
 def day1_2():
 	cnt = 0
 	with open(fileDir + '/input.txt') as file:
 		for line in file:
 			val = int(line)
-			cnt += calcFuel(val)
+			cnt += calc_fuel(val)
 	
 	return cnt
+
 
 print(day1_2())
